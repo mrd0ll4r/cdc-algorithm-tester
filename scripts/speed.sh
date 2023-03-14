d58b7428b9cd5817a5c13b3065540bc136d4f048
Marcel Gregoriadis <m.gregoriadis@gmail.com>
2023-03-14 12:16:23 +0100

    fix: missing fix with parameterized algos

--- scripts/speed.sh
+++ scripts/speed.sh
@@ -52,7 +52,7 @@ for algo in "${ALGOS[@]}"; do
           time=$(time_to_ms $time)
           header=$(printf "%s,%s,%d,%d,%d" $(get_algo_name "$subalgo") ${dataset%%.*} $dataset_size $cs $i)
           echo $header,$time
-          perf_result=$( perf stat -x, -e task-clock,context-switches,page-faults,cycles,instructions,branches,branch-misses,L1-dcache-loads,L1-dcache-misses,cache-references,cache-misses $(get_cmd $subalgo $dataset $cs) 2> /dev/stdout 1> /dev/null | awk -F',' '{print $3","$1}')
+          perf_result=$( perf stat -x, -e task-clock,context-switches,page-faults,cycles,instructions,branches,branch-misses,L1-dcache-loads,L1-dcache-misses,cache-references,cache-misses $(get_cmd "$subalgo" $dataset $cs) 2> /dev/stdout 1> /dev/null | awk -F',' '{print $3","$1}')
           for l in $perf_result; do echo $header,$l >> perf.csv; done
         done
 
