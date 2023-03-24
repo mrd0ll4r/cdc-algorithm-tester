7b4469d802f9cf2218b9cd5c59d6f0902abbe1ca
Leo Balduf <leobalduf@gmail.com>
2023-03-24 12:13:50 +0100

    bfbc: adjust min chunk size for calculations

--- scripts/bfbc-divisors.sh
+++ scripts/bfbc-divisors.sh
@@ -5,7 +5,7 @@ source scripts/utils.sh
 echo "dataset,chunk_size,divisors"
 for dataset in "${DATASETS[@]}"; do
     for chunk_size in "${TARGET_CHUNK_SIZES[@]}"; do
-        divisors="$(python3 scripts/get-bfbc-divisors.py "$dataset" "$chunk_size" 0)"
+        divisors="$(python3 scripts/get-bfbc-divisors.py "$dataset" "$chunk_size" 2)"
         echo "$dataset,$chunk_size,$divisors"
     done
 done
