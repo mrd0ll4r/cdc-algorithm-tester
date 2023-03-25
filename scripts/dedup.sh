a0db2a0d8df6dd150ea52ac1081eefb2ab1f4cf0
Leo Balduf <leobalduf@gmail.com>
2023-03-25 22:42:41 +0100

    scripts: make dedup script report sizes instead of fraction

--- scripts/dedup.sh
+++ scripts/dedup.sh
@@ -9,10 +9,11 @@ get_cmd() {
 }
 
 # CSV header
-echo "algorithm,dataset,target_chunk_size,dedup_ratio"
+echo "algorithm,dataset,dataset_size,target_chunk_size,unique_chunks_size_sum"
 
 for dataset in "${DATASETS[@]}"; do
   dataset_name="${dataset%%.*}"
+  dataset_size=$(stat -c "%s" "$DATA_PATH/$dataset")
 
   for algo in "${ALGOS[@]}"; do
     readarray -t subalgos < <(get_subalgos "$algo")
@@ -21,12 +22,12 @@ for dataset in "${DATASETS[@]}"; do
 
       # for each target chunk size
       for cs in "${TARGET_CHUNK_SIZES[@]}"; do
-        prefix=$(printf "%s,%s,%d" "$algo_name" "$dataset_name" "$cs")
+        prefix=$(printf "%s,%s,%d,%d" "$algo_name" "$dataset_name" "$dataset_size" "$cs")
         cmd=$(get_cmd "$subalgo" "$dataset" "$cs")
 
-        # cut last chunk | 1 - (sum of lengths of unique chunks) / (sum of lengths of all chunks)
-        dedup_ratio=$($cmd | head -n -1 | awk -F, '{sums[$1]=$2; total+=$2} END {for (f in sums) {unique_sum+=sums[f]; count++}; printf "%.5f\n", 1-unique_sum/total}')
-        echo "$prefix,$dedup_ratio"
+        # cut last chunk | sum of lengths of all unique chunks
+        chunk_size_sum=$($cmd | head -n -1 | sort -u | awk -F, '{print $2}' | paste -sd+ | bc)
+        echo "$prefix,$chunk_size_sum"
       done
     done
   done
