01f72feb95e4e4218256674994017b353c9d8440
Marcel Gregoriadis <m.gregoriadis@gmail.com>
2023-03-25 23:51:41 +0100

    fix

--- scripts/get-pdf.sh
+++ scripts/get-pdf.sh
@@ -2,10 +2,14 @@
 
 source scripts/utils.sh
 
+mkdir -p "$DATA_PATH/pdf"
+cd "$DATA_PATH/pdf"
+
 for i in {01..29}; do
-  aws s3 cp --request-payer requester "s3://arxiv/pdf/arXiv_pdf_1801_0$i.tar" "$DATA_PATH/pdf/."
+  aws s3 cp --request-payer requester s3://arxiv/pdf/arXiv_pdf_1801_0$i.tar .
 done
 
 echo "Creating TAR archive..."
-cat "$DATA_PATH/pdf/*.tar" > "$DATA_PATH/pdf.tar"
-rm -r "$DATA_PATH/pdf"
+cat *.tar > ../pdf.tar
+cd ..
+rm -r pdf
