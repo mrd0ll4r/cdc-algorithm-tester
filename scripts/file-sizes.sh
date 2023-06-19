1d52c2aca6965b50343091746c95d8abdbb10c88
Marcel Gregoriadis <m.gregoriadis@gmail.com>
2023-06-19 09:33:23 +0300

    fixed dataset path in file-sizes.sh

--- scripts/file-sizes.sh
+++ scripts/file-sizes.sh
@@ -3,4 +3,4 @@
 # This script requires an env variable DATASET set and will print a comma separated list of file sizes within this dataset.
 
 source scripts/utils.sh
-tar -tvf "$DATA_PATH/$DATASET.tar" | awk '$1 ~ /^-/{print $3}' | paste -sd, -
+tar -tvf "$DATA_PATH/$DATASET" | awk '$1 ~ /^-/{print $3}' | paste -sd, -
