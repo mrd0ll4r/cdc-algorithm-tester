52f77f7e4972f0d6557192ab42a0aec19dd46ec1
Marcel Gregoriadis <m.gregoriadis@gmail.com>
2023-06-19 12:49:21 +0300

    do not gzip file sizes, added plot for file sizes in datasets

--- plotting/cdc_eval.R
+++ plotting/cdc_eval.R
@@ -524,6 +524,48 @@ print_plot(p,"hash_value_distribution_rabin64_2048")
 
 
 ######################################################################
+# FILE SIZES IN DATASETS
+######################################################################
+sizes_data_code <- scan(sprintf("%s/sizes_code.txt",csv_dir), what=numeric(), sep=",")
+sizes_data_web <- scan(sprintf("%s/sizes_web.txt",csv_dir), what=numeric(), sep=",")
+sizes_data_pdf <- scan(sprintf("%s/sizes_pdf.txt",csv_dir), what=numeric(), sep=",")
+sizes_data_lnx <- scan(sprintf("%s/sizes_lnx.txt",csv_dir), what=numeric(), sep=",")
+
+# Calculate the CDFs
+cdf_func_code <- ecdf(sizes_data_code)
+cdf_func_web <- ecdf(sizes_data_web)
+cdf_func_pdf <- ecdf(sizes_data_pdf)
+cdf_func_lnx <- ecdf(sizes_data_lnx)
+
+# Create data frames with the unique values and their corresponding CDF values
+cdf_data_code <- data.frame(values = unique(sizes_data_code), cdf_values = cdf_func_code(unique(sizes_data_code)), category = "code")
+cdf_data_web <- data.frame(values = unique(sizes_data_web), cdf_values = cdf_func_web(unique(sizes_data_web)), category = "web")
+cdf_data_pdf <- data.frame(values = unique(sizes_data_pdf), cdf_values = cdf_func_pdf(unique(sizes_data_pdf)), category = "pdf")
+cdf_data_lnx <- data.frame(values = unique(sizes_data_lnx), cdf_values = cdf_func_lnx(unique(sizes_data_lnx)), category = "lnx")
+
+# Combine data frames
+cdf_data <- rbind(cdf_data_code, cdf_data_web, cdf_data_pdf, cdf_data_lnx)
+
+# Define the breaks and labels
+breaks <- c(512, 8*1024, 16*8*1024, 16*16*8*1024, 16*16*16*8*1024, 16*16*16*16*8*1024)
+labels <- c("512 B", "8 KB", "128 KB", "2 MB", "32 MB", "512 MB")
+
+# Create the plot with log scale on x-axis
+p <- ggplot(cdf_data, aes(x = values, y = cdf_values, color = category)) +
+  geom_line() +
+  scale_x_log10(breaks = breaks, labels = labels) +
+  geom_vline(xintercept = 512, linetype = "dashed") +
+  xlab("File Size (Log Scale)") +
+  ylab("Cumulative Probability") +
+  labs(color = "Dataset") +
+  ggtitle("Cumulative Distribution Function (CDF) with Log Scale")
+
+print_plot(p,"dataset_file_sizes")
+
+gc()
+
+
+######################################################################
 # CHUNK SIZE DISTRIBUTIONS
 ######################################################################
 # Note: The complete CSD dataset is too large to be stored in one R vector.
--- scripts/run-all-experiments.sh
+++ scripts/run-all-experiments.sh
@@ -51,10 +51,10 @@ done
 
 echo "Analyzing file sizes..."
 
-bash -c 'DATASET="code.tar" make sizes | gzip -9 > csv/sizes_code.csv.gz' &
-bash -c 'DATASET="web.tar" make sizes | gzip -9 > csv/sizes_web.csv.gz' &
-bash -c 'DATASET="pdf.tar" make sizes | gzip -9 > csv/sizes_pdf.csv.gz' &
-bash -c 'DATASET="lnx.tar" make sizes | gzip -9 > csv/sizes_lnx.csv.gz' &
+bash -c 'DATASET="code.tar" make sizes > csv/sizes_code.txt' &
+bash -c 'DATASET="web.tar" make sizes > csv/sizes_web.txt' &
+bash -c 'DATASET="pdf.tar" make sizes > csv/sizes_pdf.txt' &
+bash -c 'DATASET="lnx.tar" make sizes > csv/sizes_lnx.txt' &
 
 
 ########################
