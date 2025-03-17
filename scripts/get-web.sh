62268dccdfae34858ea6d6f18dc348bc3462a7ee
Marcel Gregoriadis <m.gregoriadis@tudelft.nl>
2025-03-17 11:57:52 +0100

    adjust get-web.sh depth

--- scripts/get-web.sh
+++ scripts/get-web.sh
@@ -14,7 +14,7 @@ month=01
 days_in_month=$(LC_ALL=C cal -d "${year}-${month}-01" | awk 'NF {DAYS = $NF}; END {print DAYS}')
 for day in $(seq -w 1 "$days_in_month"); do
   echo "downloading https://web.archive.org/web/${year}${month}${day}id_/https://nytimes.com"
-  wget -E -H -k -p -r -l3 -q --timeout=30 --tries=2 -P "${year}${month}${day}" "https://web.archive.org/web/${year}${month}${day}id_/https://nytimes.com" &
+  wget -E -H -k -p -r -l1 -q --timeout=30 --tries=2 -P "${year}${month}${day}" "https://web.archive.org/web/${year}${month}${day}id_/https://nytimes.com" &
 done
 
 wait
