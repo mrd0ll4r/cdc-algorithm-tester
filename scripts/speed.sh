4486c4510bc7886ed7c5877efde3801f57843842
Leo Balduf <leobalduf@gmail.com>
2025-09-01 10:30:48 +0200

    scripts: update speed for single-dataset single-file situation

--- scripts/speed.sh
+++ scripts/speed.sh
@@ -17,35 +17,35 @@ fi
 # CSV header
 echo "algorithm,dataset,dataset_size,target_chunk_size,iteration,event,value"
 
-for dataset in "${DATASETS[@]}"; do
-  # copy dataset to ramdisk
-  cp "$DATA_PATH/$dataset" "$FAST_DATA_PATH/"
+# copy dataset to ramdisk
+cp "$DATA_PATH/random/random.bin" "$FAST_DATA_PATH/"
+dataset="random.bin"
 
-  dataset_size=$(stat -c "%s" "$FAST_DATA_PATH/$dataset")
+dataset_size=$(stat -c "%s" "$FAST_DATA_PATH/$dataset")
 
-  for algo in "${ALGOS[@]}"; do
-    readarray -t subalgos < <(get_subalgos "$algo")
-    for subalgo in "${subalgos[@]}"; do
+for algo in "${ALGOS[@]}"; do
+  readarray -t subalgos < <(get_subalgos "$algo")
+  for subalgo in "${subalgos[@]}"; do
 
-      # warm-up run
-      $(get_cmd "$subalgo" "$dataset" "${TARGET_CHUNK_SIZES[0]}") >/dev/null
+    # warm-up run
+    $(get_cmd "$subalgo" "$dataset" "${TARGET_CHUNK_SIZES[0]}") >/dev/null
 
-      # produce result for each chunk size
-      for cs in "${TARGET_CHUNK_SIZES[@]}"; do
-        cmd=$(get_cmd "$subalgo" $dataset $cs)
-        for i in $(seq 1 $ITER); do
-          header=$(printf "%s,%s,%d,%d,%d" $(get_algo_name "$subalgo") "${dataset%%.*}" "$dataset_size" "$cs" "$i")
-          perf_result=$(perf stat -x, -e "$perf_events_to_collect" $cmd 2>/dev/stdout 1>/dev/null | awk -F',' '{print $3","$1}')
-          for l in $perf_result; do echo "$header,$l"; done
-        done
-
-        # as nop is agnostic to target chunk sizes, one chunk size is enough
-        if [ "$algo" = "nop" ]; then
-          break
-        fi
+    # produce result for each chunk size
+    for cs in "${TARGET_CHUNK_SIZES[@]}"; do
+      cmd=$(get_cmd "$subalgo" $dataset $cs)
+      for i in $(seq 1 $ITER); do
+        header=$(printf "%s,%s,%d,%d,%d" $(get_algo_name "$subalgo") "${dataset%%.*}" "$dataset_size" "$cs" "$i")
+        perf_result=$(perf stat -x, -e "$perf_events_to_collect" $cmd 2>/dev/stdout 1>/dev/null | awk -F',' '{print $3","$1}')
+        for l in $perf_result; do echo "$header,$l"; done
       done
+
+      # as nop is agnostic to target chunk sizes, one chunk size is enough
+      if [ "$algo" = "nop" ]; then
+        break
+      fi
     done
   done
-  # remove dataset from ramdisk to free space
-  rm "$FAST_DATA_PATH/$dataset"
 done
+
+# remove dataset from ramdisk to free space
+rm "$FAST_DATA_PATH/$dataset"
